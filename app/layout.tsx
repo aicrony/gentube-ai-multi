5bf49a7e30f8a0cbc32ace5174d4bf8e9591cd29
Jason Cabungcal <jasoncabungcal@gmail.com>
2024-11-07 00:24:18 -0500

    GENTUBE-MULTI - Add Analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from '@/components/ui/Navbar';
 import { Toaster } from '@/components/ui/Toasts/toaster';
 import { PropsWithChildren, Suspense } from 'react';
 import { getURL } from '@/utils/helpers';
+import Head from 'next/head';
 import 'styles/main.css';
 
 const title = 'Next.js Subscription Starter';
@@ -21,20 +22,33 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
+      <html lang="en">
+        <Head>
+          <title>Generate images and videos with AI.</title>
+          {/* Google tag (gtag.js) */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-634FFY459F"></script>
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-634FFY459F');
+            `}
+          </script>
+        </Head>
       <body className="bg-black">
         <Navbar />
-        <main
-          id="skip"
-          className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
-        >
-          {children}
-        </main>
+          <main
+              id="skip"
+              className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
+          >
+            {children}
+          </main>
         <Footer />
         <Suspense>
           <Toaster />
         </Suspense>
       </body>
-    </html>
+      </html>
   );
 }
