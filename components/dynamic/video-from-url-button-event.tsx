adb79cd8466e832f30f7a1f4b0e1bc4045813ff6
Jason Cabungcal <jasoncabungcal@gmail.com>
2024-12-17 00:43:07 -0500

    GENTUBE-MULTI - Video limit message is now passed to the view.

--- components/dynamic/video-from-url-button-event.tsx
+++ components/dynamic/video-from-url-button-event.tsx
@@ -18,12 +18,14 @@ export function VideoFromUrlDynamicButton({
   const [imageUrl, setImageUrl] = useState<string>('');
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [videoDescription, setVideoDescription] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const handleGenerateVideo = async () => {
     setIsSubmitting(true); // Disable the button while the request is being handled
     console.log('Video Generation from URL button clicked');
     console.log(imageUrl);
     setVideoData(null); // clear the videoData state
+    setErrorMessage(null); // clear any previous error message
     try {
       const response = await fetch('/api/video', {
         method: 'POST',
@@ -39,7 +41,14 @@ export function VideoFromUrlDynamicButton({
       });
       if (!response.ok) {
         setIsSubmitting(false); // Response is received, enable the button
-        throw new Error(`HTTP error! status: ${response.status}`);
+        if (response.status === 429) {
+          setErrorMessage(
+            'Daily VIDEO request limit exceeded. Please subscribe on the PRICING page.'
+          );
+        } else {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return;
       }
       let data = {};
       if (response.headers) {
@@ -57,53 +66,58 @@ export function VideoFromUrlDynamicButton({
   };
 
   return (
-    <div className={'pt-5'}>
-      <div className="grid gap-2">
-        <Label htmlFor="imageUrl">
-          Enter a URL of an image to start creating your video.
-        </Label>
-        <Input
-          as="textarea"
-          id="imageUrl"
-          value={imageUrl}
-          onChange={(e) => setImageUrl(e.target.value)}
-          className="min-h-[25px] text-xl"
-          placeholder="Enter image URL"
-        />
-      </div>
-      <div className={'pt-4'}>
-        <Input
-          type="text"
-          placeholder={'What will happen in the video?'}
-          className="min-h-[25px] text-xl"
-          onChange={(e) => setVideoDescription(e.target.value)}
-        />
-      </div>
-      <div className={'pt-4'}>
-        <Button
-          variant="slim"
-          type="submit"
-          className="mt-1"
-          loading={isSubmitting}
-          onClick={handleGenerateVideo}
-        >
-          Generate Video from Image URL
-        </Button>
-      </div>
-      {videoData && (
-        <div className={'padding-top-4'}>
-          <p>Video Generation Complete</p>
-          <div>
-            <video width="600" controls>
-              <source src={videoData} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </div>
-          <div>
-            <Downloader fileUrl={videoData} />
-          </div>
-        </div>
+    <>
+      {errorMessage && (
+        <div className="error-message-large">{errorMessage}</div>
       )}
-    </div>
+      <div className={'pt-5'}>
+        <div className="grid gap-2">
+          <Label htmlFor="imageUrl">
+            Enter a URL of an image to start creating your video.
+          </Label>
+          <Input
+            as="textarea"
+            id="imageUrl"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            className="min-h-[25px] text-xl"
+            placeholder="Enter image URL"
+          />
+        </div>
+        <div className={'pt-4'}>
+          <Input
+            type="text"
+            placeholder={'What will happen in the video?'}
+            className="min-h-[25px] text-xl"
+            onChange={(e) => setVideoDescription(e.target.value)}
+          />
+        </div>
+        <div className={'pt-4'}>
+          <Button
+            variant="slim"
+            type="submit"
+            className="mt-1"
+            loading={isSubmitting}
+            onClick={handleGenerateVideo}
+          >
+            Generate Video from Image URL
+          </Button>
+        </div>
+        {videoData && (
+          <div className={'padding-top-4'}>
+            <p>Video Generation Complete</p>
+            <div>
+              <video width="600" controls>
+                <source src={videoData} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+            <div>
+              <Downloader fileUrl={videoData} />
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
