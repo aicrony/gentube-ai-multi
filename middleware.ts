fb4d0b2f7c65c7ccdb9005e3c228f1c2391afa5c
Jason Cabungcal <jasoncabungcal@gmail.com>
2024-12-19 00:30:30 -0500

    Revert "GENTUBE-MULTI - Redirect updated to be invalid for testing purposes"
    This reverts commit cabf9ce820397379f6d43894863703b6d8945a11.

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { updateSession } from '@/utils/supabase/middleware';
 export async function middleware(request: NextRequest) {
   const mainUrl = 'https://gentube.ai';
 
-  if (request.nextUrl.hostname.endsWith('.vercelo.app')) {
+  if (request.nextUrl.hostname.endsWith('.vercel.app')) {
     return NextResponse.redirect(mainUrl);
   }
 
