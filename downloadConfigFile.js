b88eb8cb4d3c4432acc09b86b54e0505a981bab2
Jason Cabungcal <jasoncabungcal@gmail.com>
2024-12-28 15:53:50 -0500

    GENTUBE-MULTI - Prebuild failures corrections

--- downloadConfigFile.js
+++ downloadConfigFile.js
@@ -1,22 +1,32 @@
 const { Storage } = require('@google-cloud/storage');
+const fs = require('fs');
 const path = require('path');
 
 const bucketName = process.env.GOOGLE_CONFIG_BUCKET_NAME;
 const srcFilename = process.env.GOOGLE_CONFIG_FILE_NAME;
 const destFilename = path.join(
   __dirname,
-  process.env.GOOGLE_APPLICATION_CREDENTIALS
+  'local-cedar-324921-5c8f531afb2e.json'
 );
 
-async function downloadFile() {
+async function downloadAndWriteFile() {
   const storage = new Storage();
   const options = {
-    destination: destFilename
+    destination: '/tmp/tempfile'
   };
 
+  // Download the file to a temporary location
   await storage.bucket(bucketName).file(srcFilename).download(options);
 
-  console.log(`Downloaded ${srcFilename} to ${destFilename}`);
+  // Read the downloaded file
+  const fileContent = fs.readFileSync('/tmp/tempfile', 'utf8');
+
+  // Write the content to the existing file
+  fs.writeFileSync(destFilename, fileContent, 'utf8');
+
+  console.log(
+    `Downloaded ${srcFilename} and wrote its content to ${destFilename}`
+  );
 }
 
-downloadFile().catch(console.error);
+downloadAndWriteFile().catch(console.error);
