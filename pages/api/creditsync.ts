8877bd22ab7a709d2bf3773e518e608acc3c574f
Jason Cabungcal <jasoncabungcal@gmail.com>
2025-01-11 00:21:36 -0500

    GENTUBE-MULTI - creditsync api

---
+++ pages/api/creditsync.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { updateUserCredits } from '@/utils/gcloud/userCredits';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    res.status(405).end(); // Method Not Allowed
+    console.error('Method Not Allowed on /api/creditsync');
+    return;
+  }
+
+  try {
+    await updateUserCredits('1234', '-', 17001);
+    res.status(200).json({ received: true });
+  } catch (error) {
+    console.error('Sync Webhook handler failed.', error);
+    res.status(500).json({ error: 'Sync Webhook handler failed.' });
+  }
+}
