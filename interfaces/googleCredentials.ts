ff79e5a25d7654afa3626e8cab1599686a017f7f
Jason Cabungcal <jasoncabungcal@gmail.com>
2025-01-25 22:42:07 -0500

    GENTUBE-MULTI - Change env var names

--- interfaces/googleCredentials.ts
+++ interfaces/googleCredentials.ts
@@ -5,26 +5,26 @@ dotenv.config();
 
 export interface GoogleAppCreds {
   type: string | undefined;
-  project_id: string | undefined;
-  private_key_id: string | undefined;
+  projectId: string | undefined;
+  privateKeyId: string | undefined;
   private_key: string | undefined;
   client_email: string | undefined;
-  client_id: string | undefined;
-  auth_uri: string | undefined;
-  token_uri: string | undefined;
-  auth_provider_x509_cert_url: string | undefined;
-  client_x509_cert_url: string | undefined;
+  clientId: string | undefined;
+  authUri: string | undefined;
+  tokenUri: string | undefined;
+  authProviderX509CertUrl: string | undefined;
+  clientX509CertUrl: string | undefined;
 }
 
 export const google_app_creds: GoogleAppCreds = {
   type: process.env.TYPE,
-  project_id: process.env.PROJECT_ID,
-  private_key_id: process.env.PRIVATE_KEY_ID,
+  projectId: process.env.PROJECT_ID,
+  privateKeyId: process.env.PRIVATE_KEY_ID,
   private_key: process.env.PRIVATE_KEY,
   client_email: process.env.CLIENT_EMAIL,
-  client_id: process.env.CLIENT_ID,
-  auth_uri: process.env.AUTH_URI,
-  token_uri: process.env.TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+  clientId: process.env.CLIENT_ID,
+  authUri: process.env.AUTH_URI,
+  tokenUri: process.env.TOKEN_URI,
+  authProviderX509CertUrl: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  clientX509CertUrl: process.env.CLIENT_X509_CERT_URL
 };
