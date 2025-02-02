ede4e5d9743fa59f83829994bd9f6ff066dfbb53
Jason Cabungcal <jasoncabungcal@gmail.com>
2025-02-02 10:22:33 -0500

    GENTUBE-MULTI - Increase modal size on Gallery

--- components/ui/GenericModal/GenericModal.tsx
+++ components/ui/GenericModal/GenericModal.tsx
@@ -20,7 +20,7 @@ const GenericModal: React.FC<ModalProps> = ({ children, isOpen, onClose }) => {
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
       onClick={handleBackgroundClick}
     >
-      <div className="bg-gray-200 p-4 rounded shadow-lg max-w-lg w-full max-h-screen overflow-y-auto relative">
+      <div className="bg-gray-200 p-4 rounded shadow-lg max-w-4xl w-full max-h-screen overflow-y-auto relative">
         <button
           onClick={onClose}
           className="absolute top-0 right-0 text-gray-500 hover:text-gray-700 text-2xl"
