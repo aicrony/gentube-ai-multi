bbb0829d09ca6ef64f45dd8898fa5e103725555c
Jason Cabungcal <jasoncabungcal@gmail.com>
2025-02-02 12:58:32 -0500

    GENTUBE-MULTI - modify to 3 instead of 4 xl

--- components/ui/Modal/Modal.tsx
+++ components/ui/Modal/Modal.tsx
@@ -17,7 +17,7 @@ const Modal: React.FC<ModalProps> = ({ mediaUrl, onClose }) => {
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
       onClick={handleBackgroundClick}
     >
-      <div className="bg-white p-4 rounded shadow-lg max-w-4xl w-full relative">
+      <div className="bg-white p-4 rounded shadow-lg max-w-3xl w-full relative">
         <button
           onClick={onClose}
           className="absolute top-0 right-0 text-gray-500 hover:text-gray-700 text-2xl"
