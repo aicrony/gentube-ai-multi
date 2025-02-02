28b9dacd74125cafb47dc2959bd18cfdff9393a9
Jason Cabungcal <jasoncabungcal@gmail.com>
2025-02-01 21:46:00 -0500

    GENTUBE-MULTI - Fix header padding

--- components/ui/Navbar/Navlinks.tsx
+++ components/ui/Navbar/Navlinks.tsx
@@ -16,12 +16,12 @@ export default function Navlinks({ user }: NavlinksProps) {
   const router = getRedirectMethod() === 'client' ? useRouter() : null;
 
   return (
-    <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
+    <div className="relative flex flex-row justify-between py-1 align-center md:py-6">
       <div className="flex items-center flex-1">
         {/*<Link href="/" className={s.logo} aria-label="Logo">*/}
         {/*  <Logo />*/}
         {/*</Link>*/}
-        <nav className="ml-6 space-x-2 lg:block">
+        <nav className="ml-4 space-x-1 lg:block">
           <Link href="/" target="_parent" className={s.link}>
             Home
           </Link>
