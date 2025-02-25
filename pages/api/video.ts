a8ad2a827a3d0ba773a3e9dbae56e620cd1ebf3d
Jason Cabungcal <jasoncabungcal@gmail.com>
2025-02-24 23:20:07 -0500

    GENTUBE-MULTI - Refactor image generation.

--- pages/api/video.ts
+++ pages/api/video.ts
@@ -2,14 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import callRay2VideoApi from '@/services/generateFrontierLumaVideo';
 import { serialize } from 'cookie';
 import { saveUserActivity } from '@/utils/gcloud/saveUserActivity';
-import {
-  processUserImageRequest,
-  updateUserCredits
-} from '@/utils/gcloud/processUserImageRequest';
+import { updateUserCredits } from '@/utils/gcloud/processUserImageRequest';
 import generateFalVideo from '@/services/generateFalVideo';
 import { getSubscriptionTier } from '@/functions/getSubscriptionTier';
-import generateLumaVideo from '@/services/generateLumaVideo';
-import generateFrontierLumaVideo from '@/services/generateFrontierLumaVideo';
+import { getUserCredits } from '@/pages/api/getUserCredits';
 
 export default async function handler(
   req: NextApiRequest,
@@ -30,7 +26,7 @@ export default async function handler(
   const initialCredits = subscriptionObject.initialCredits;
 
   // Get user credits from the new table
-  let userCredits = await userCredits(userId, userIp);
+  let userCredits = await getUserCredits(userId, userIp);
 
   if (userCredits === null) {
     userCredits = initialCredits;
