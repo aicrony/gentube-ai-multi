25426cd0f55359d7ba7462a079758356348c700b
Jason Cabungcal <jasoncabungcal@gmail.com>
2025-03-02 13:43:55 -0500

    Revert "GENTUBE-MULTI - Correct new user query"
    This reverts commit 2be2a955dbc322b5fd377fa406cdebab03e26e31.

--- utils/gcloud/processUserImageRequest.ts
+++ utils/gcloud/processUserImageRequest.ts
@@ -29,7 +29,7 @@ export async function processUserImageRequest(
     };
     url?: string;
   };
-  if (userId && userId != 'none' && userId.length > 0) {
+  if (userId != undefined && userId.length > 0) {
     console.log('Query 1');
     query = datastore
       .createQuery(namespace, kind)
